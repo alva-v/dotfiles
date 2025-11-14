9be25c1b709e8f57f4b8c3d88525af9abeb5a92f
Alva Vially <alva@vially.fr>
2025-11-14 16:04:39 +0100

    Force Firefox extensions

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,8 +1,19 @@
 #! /bin/bash
 
+set -e
+
+install_firefox_policies() {
+    sudo mkdir -p /etc/firefox-esr/policies
+    sudo mkdir -p /etc/firefox/policies
+    cwd=$(pwd)
+    sudo ln -s "$cwd/firefox-policies.json" /etc/firefox-esr/policies/policies.json
+    sudo ln -s "$cwd/firefox-policies.json" /etc/firefox/policies/policies.json
+}
+
 if [[ $(realpath ..) != "$HOME" ]]; then
     echo "This dotfiles repo should be located in a subfolder of $HOME (eg. $HOME/.dotfiles)"
     exit 1
 fi
 
 stow --verbose=2 .
+install_firefox_policies
