3ecc9a5855e972c5bcd4604586f172fc6fc344ed
Alva Vially <alva@vially.fr>
2025-11-20 11:01:11 +0100

    Move relevant files to ./non-home/ and add Codium extensions

--- Readme.md
+++ Readme.md
@@ -5,6 +5,8 @@ This is a very incomplete work in progress.
 
 The `non-automated` folder contains files that I still have to set up bootstrapping logic for.
 
+The `non-home` folder contains files that are above the home folder in the folder hierarchy and config files that are to be used by the bootstrapping script without being stowed.
+
 ## Usage
 Clone this repo in `~/.dotfiles`, then run:
 ```
@@ -13,6 +15,6 @@ bash bootstrap.sh
 
 ## Notes on non-automated files
 ### Redirector.json
-Config for the firefox extension
+Config for the firefox extension.
 
 Might be possible to automatically deploy by standardizing my Firefox profile ID and inserting the data in the corresponding local storage sqlite table.
--- bootstrap.sh
+++ bootstrap.sh
@@ -2,12 +2,16 @@
 
 set -e
 
+install_codium_extensions() {
+    xargs -L 1 codium --install-extension < ./non-home/codium_extensions
+}
+
 install_firefox_policies() {
     sudo mkdir -p /etc/firefox-esr/policies
     sudo mkdir -p /etc/firefox/policies
     cwd=$(pwd)
-    sudo ln -s "$cwd/firefox-policies.json" /etc/firefox-esr/policies/policies.json
-    sudo ln -s "$cwd/firefox-policies.json" /etc/firefox/policies/policies.json
+    sudo ln -s "$cwd/non-home/firefox-policies.json" /etc/firefox-esr/policies/policies.json
+    sudo ln -s "$cwd/non-home/firefox-policies.json" /etc/firefox/policies/policies.json
 }
 
 if [[ $(realpath ..) != "$HOME" ]]; then
@@ -17,3 +21,4 @@ fi
 
 stow --verbose=2 .
 install_firefox_policies
+install_codium_extensions
