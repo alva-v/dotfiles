d808d927f32409a7cf4218130ee16becc589ec3f
Alva Vially <alva@vially.fr>
2026-01-16 14:31:23 +0100

    Allow bootstrapping from another folder

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,7 +2,18 @@
 
 set -e
 
+# VARIABLES
+script_dir=$(dirname "$0")
+parent_dir=$(realpath "$(dirname "$script_dir")")
+
 # FUNCTIONS
+check_dir() {
+    if [[ "$parent_dir" != "$HOME" ]]; then
+        echo "This dotfiles repo should be located in a subfolder of $HOME (eg. $HOME/.dotfiles)"
+        exit 1
+    fi
+}
+
 check_not_root() {
     if [[ $EUID -eq 0 ]]; then
         echo "Don't run this script as root"
@@ -11,24 +22,19 @@ check_not_root() {
 }
 
 install_codium_extensions() {
-    xargs -L 1 codium --install-extension < ./non-home/codium-extensions
+    xargs -L 1 codium --install-extension < "$0"/non-home/codium-extensions
 }
 
 install_firefox_policies() {
     sudo mkdir -p /etc/firefox-esr/policies
     sudo mkdir -p /etc/firefox/policies
-    cwd=$(pwd)
-    sudo ln -s "$cwd/non-home/firefox-policies.json" /etc/firefox-esr/policies/policies.json
-    sudo ln -s "$cwd/non-home/firefox-policies.json" /etc/firefox/policies/policies.json
+    sudo ln -s "$script_dir/non-home/firefox-policies.json" /etc/firefox-esr/policies/policies.json
+    sudo ln -s "$script_dir/non-home/firefox-policies.json" /etc/firefox/policies/policies.json
 }
 
 # SCRIPT
 check_not_root
-if [[ $(realpath ..) != "$HOME" ]]; then
-    echo "This dotfiles repo should be located in a subfolder of $HOME (eg. $HOME/.dotfiles)"
-    exit 1
-fi
-
-stow --adopt --verbose=2 .
+check_dir
+stow --adopt --verbose=2 --dir="$script_dir" .
 install_firefox_policies
 install_codium_extensions
